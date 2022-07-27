e14470e3b2fb886e6fe3bc7d17148e77a3cab235
nissi15 <ishemanissi228@gmail.com>
2022-07-28 08:40:47 +1000

    first

--- projects/models.py
+++ projects/models.py
@@ -10,7 +10,7 @@ class Project(models.Model):
     source_link = models.CharField(max_length= 2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True) 
-    vote_total = models.IntegerField(default=0, null=True, blank=True) 
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True) 
     created = models.DateField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
@@ -30,7 +30,7 @@ class Review(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False) 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) 
 
 
 
@@ -41,13 +41,13 @@ class Review(models.Model):
 
 
     
-class Tag(models.Model):
+class Tag(models.Model): 
     name = models.CharField(max_length=200)
     created = models.DateField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
 
     def __str__(self):
-        return self.value
+        return self.name
 
 
 
