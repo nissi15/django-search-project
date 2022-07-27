1d602c55ea87d01de433fbe48c257de4c1aa14fe
nissi15 <ishemanissi228@gmail.com>
2022-07-27 12:40:51 +1000

    yrul

---
+++ projects/views.py
@@ -0,0 +1,41 @@
+from multiprocessing import context
+from django.shortcuts import render
+from django.http import HttpResponse
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source built by the community'
+    }
+]
+
+
+def projects(request):
+    page = 'projects'
+    number = 10 
+
+    context = {'page': page, 'number': number, 'projects':projectsList}
+
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk): 
+    projectobj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectobj = i
+    return render(request, 'projects/single-project.html', {'project':projectobj})
+
+
+
